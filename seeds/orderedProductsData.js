720822687dcc1e99498a0e706c6e6874cd41f198
BranBao1995 <brandon.0702@hotmail.com>
2022-10-27 10:41:39 -0400

    added seed files for testing

--- seeds/orderedProductsData.js
+++ seeds/orderedProductsData.js
@@ -3,35 +3,35 @@ const { OrderedProducts } = require("../models");
 const orderedproductsdata = [
   {
     product_id: 1,
-    user_id: 1,
+    order_id: 1,
   },
   {
     product_id: 4,
-    user_id: 1,
+    order_id: 1,
   },
   {
     product_id: 2,
-    user_id: 2,
+    order_id: 2,
   },
   {
     product_id: 5,
-    user_id: 2,
+    order_id: 2,
   },
   {
     product_id: 6,
-    user_id: 3,
+    order_id: 3,
   },
   {
     product_id: 1,
-    user_id: 3,
+    order_id: 3,
   },
   {
     product_id: 3,
-    user_id: 4,
+    order_id: 4,
   },
   {
     product_id: 8,
-    user_id: 4,
+    order_id: 4,
   },
 ];
 
