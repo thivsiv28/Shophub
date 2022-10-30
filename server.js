2d746a4183942e2a4c29c5ccc40a0aee9b4da209
BranBao1995 <brandon.0702@hotmail.com>
2022-10-30 00:25:14 -0400

    added css styling (not done yet)

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const hbs = exphbs.create({ helpers });
 const sess = {
   secret: "Super secret secret",
   cookie: {
-    maxAge: 60000,
+    maxAge: 6000000,
     httpOnly: true,
     secure: false,
     sameSite: "strict",
